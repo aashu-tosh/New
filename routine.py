1ac7e6da79bc6e65891e213c99629906dffffee6
aashu-tosh <39301912+aashu-tosh@users.noreply.github.com>
2018-05-15 17:30:49 +0545

    Add files via upload

---
+++ routine.py
@@ -0,0 +1,26 @@
+day=int(input("""Select the day:
+                 1. Sunday
+                 2. Monday
+                 3. Tuesday
+                 4. Wednesday
+                 5. Thursday
+                 6. Friday
+
+                 Day : """))
+days = ['Invalid','Sunday','Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday']
+
+
+Sunday=['asdf','sdf','asadf']
+Invalid=['asdf','sdf','asadf']
+Monday=['asdf','sdf','asadf']
+Tuesday=['asdf','sdf','asadf']
+Wednesday=['asdf','sdf','asadf']
+Thursday=['asdf','sdf','asadf']
+Friday=['asdf','sdf','asadf']
+
+daysa = [Invalid,Sunday,Monday,Tuesday,Wednesday,Thursday,Friday]
+print("{0:>150}'s Routine".format(days[day]))
+
+
+
+print(daysa[day])
